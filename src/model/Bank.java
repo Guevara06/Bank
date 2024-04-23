ab0b1be2e9ef54581fb09160a633174949fb6ca6
unknown <estefayiso@gmail.com>
2024-04-23 12:20:32 -0500

    Feat:create bank methods

--- src/model/Bank.java
+++ src/model/Bank.java
@@ -11,9 +11,28 @@ public class Bank {
 
     public String makeTransferBetweenAccounts(Long numberAccount1 , Long numberAccount2 , double value ){
         String message = "";
-
-        return message;
+        if (numberAccount1== account1.getNumberAccount()){
+            if(numberAccount2 == account2.getNumberAccount()){
+                if(account1.getAccountBalance()>= value){
+                    account1.setAccountBalance(account1.getAccountBalance() - value);
+                    account2.setAccountBalance(account2.getAccountBalance() + value);
+                    return message = "La transacción se realizó de manera exitosa";
+                }else {
+                    return message= "NO se tiene el saldo suficiente en la cuenta";
+                }
+            }else {
+                return message = "No existe una cuenta que reciba el dinero con ese número";
+            }
+        }else {
+            return message = "No existe una cuenta que envíe el dinero con ese número";
+        }
     }
 
+    public String printNumberAccounts(){
+        String message = "";
+        return message = "NÚMERO DE LAS CUENTAS ALMACENADAS" + "\n"
+                + account1.getNumberAccount() + "\n"
+                + account2.getNumberAccount();
+    }
 
 }
