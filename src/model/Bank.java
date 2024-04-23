5cd929609932888129a227985ef17a2270a60d67
unknown <estefayiso@gmail.com>
2024-04-23 10:08:14 -0500

    Feat: add Bank

---
+++ src/model/Bank.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Bank {
+    private Account account1;
+    private Account account2;
+
+    public Bank(Account account1, Account account2){
+        this.account1 = account1;
+        this.account2 = account2;
+    }
+
+    public String makeTransferBetweenAccounts(Long numberAccount1 , Long numberAccount2 , double value ){
+        String message = "";
+
+        return message;
+    }
+
+    public
+}
